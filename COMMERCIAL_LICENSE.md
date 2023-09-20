2c2c7797a51632a8ce2f97c16206ba2200d921a9
Samuel Vange <7166607+svange@users.noreply.github.com>
2023-09-20 05:03:54 -0700

    Establishing project structure, publishing, license and other metadata details.

---
+++ COMMERCIAL_LICENSE.md
@@ -0,0 +1,25 @@
+# Commercial License for OpenBra.in
+
+This is a commercial license available for OpenBrain, separate from the AGPL-3.0 license under which the project is generally available. This commercial license grants you the rights outlined below, allowing for more flexibility in how you use the code.
+
+## Terms and Conditions
+
+1. **Scope of License**: This license grants you the right to use, modify, and distribute the software in proprietary projects.
+
+2. **Attribution**: While you are not required to open-source your derivative works, attribution to the original project must be provided.
+
+3. **Warranty**: This license comes without any warranty, explicit or implied.
+
+4. **Validity**: This license remains valid until terminated by either party, provided the terms of the license are met.
+
+5. **Fee**: A license fee, agreed upon by both parties, is required for the activation of this license.
+
+6. **Termination**: The license may be terminated if any of the terms are breached, with or without notice.
+
+7. **Jurisdiction**: Legal disputes related to this license will be settled under the jurisdiction of California.
+
+For inquiries and to obtain this commercial license, please contact the project maintainer listed in PyPi or GitHub or at **admin@openbra.in**.
+
+-----
+
+This is a summary and not a replacement for the full license, which should be individually negotiated. Please consult legal advice when considering this license.
--- ci_cd.py
+++ ci_cd.py
@@ -7,7 +7,6 @@ import sys
 import openbrain
 
 import boto3
-import toml
 import pytest
 from dotenv import load_dotenv
 
@@ -21,15 +20,18 @@ CENTRAL_INFRA_STACK_NAME = os.getenv("INFRA_STACK_NAME")
 AWS_PROFILE = os.environ.get("AWS_PROFILE", "default")
 
 logger = logging.getLogger("CICD Script")
-logging.basicConfig()
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
 
-# Add this directory to the PYTHONPATH for the call to pytest
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/openbrain")
+BOTO_SESSION = boto3.Session()
+# if BOTO_SESSION.region_name is None:
+#     AWS_REGION = os.environ.get("AWS_REGION")
+#     BOTO_SESSION = boto3.Session(region_name=AWS_REGION)
+# else:
+#     AWS_REGION = BOTO_SESSION.region_name
 
-
-def run_command(command: str):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=os.environ, shell=True)
+def run_command(command: list):
+    process = subprocess.Popen(" ".join(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=os.environ,
+                               shell=True)
 
     for c in iter(lambda: process.stdout.read(1), b""):
         sys.stdout.buffer.write(c)
@@ -40,7 +42,7 @@ def run_command(command: str):
         raise SystemExit("Deployment failed.")
 
 
-def deploy_infra():
+def deploy_infra(dry_run: bool = False):
     """Deploy infrastructure stack"""
 
     commands = [
@@ -72,7 +74,7 @@ def deploy_infra():
     ]
     [command.append(f"--profile={AWS_PROFILE}") for command in commands]
 
-    if args.dry_run:
+    if dry_run:
         last_command = commands.pop()
         # We're removing the last command if this is a dry run, so fail if our assumption is broken.
         if "deploy" not in ' '.join(last_command):
@@ -87,65 +89,52 @@ def deploy_infra():
         run_command(command)
 
 
-def package_python():
-    raise NotImplementedError("package_python not implemented yet.")
+def build_python():
+    """Use poetry to build the python package."""
+    commands = [
+        ['poetry', 'lock'],
+        ['poetry', 'build'],
+    ]
+
+    for command in commands:
+        logger.debug(f"Running {' '.join(command)}")
+        run_command(command)
 
 
-def distribute_python():
-    raise NotImplementedError("distribute_python not implemented yet.")
+def publish_python(stage: str = "dev"):
+    """Use poetry to publish the python package to pypi. If stage == dev, publish to pypi-test"""
+    if stage == "dev":
+        commands = [
+            ['poetry', 'publish', '-r', 'test-pypi'],
+        ]
+    elif stage == "prod":
+        commands = [
+            ['poetry', 'publish'],
+        ]
+    else:
+        raise SystemExit(f"Unknown {stage=}")
+
+    for command in commands:
+        logger.debug(f"Running: {' '.join(command)}")
+        run_command(command)
 
 
-def format_dependency_string(name: str, details: dict) -> str:
-    version = details.get("version", "")
-    extras = f"[{','.join(details.get('extras', []))}]" if details.get("extras") else ""
-    markers = f"; {details['markers']}" if details.get("markers") else ""
-    return f"{name}{extras} = \"{version}\"{markers}"
-
-
-def update_pyproject_from_pipfile():
-    """Update pyproject.toml dependencies from Pipfile.lock"""
-
-    command = ["pipenv", "lock"]
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    while True:
-        output = process.stdout.readline()
-
-        if output:
-            print(output.strip().decode("utf-8"))
-
-        return_code = process.poll()
-
-        if return_code is not None:
-            if return_code != 0:
-                raise SystemExit(f"Error running {command=}, returned {return_code=}")
-            break
-
-    logger.info("Requirements management completed successfully!")
-
-    with open('Pipfile.lock', 'r') as f:
-        pipfile_lock_data = json.load(f)
-
-    # Extract dependencies and dev-dependencies
-    default_dependencies = pipfile_lock_data.get('default', {})
-    dev_dependencies = pipfile_lock_data.get('develop', {})
-
-    # Read pyproject.toml
-    pyproject_data = toml.load('pyproject.toml')
-
-    # Transform dependencies to TOML format
-    pyproject_data['dependencies'] = {k: format_dependency_string(k, v) for k, v in default_dependencies.items()}
-    pyproject_data['dev-dependencies'] = {k: format_dependency_string(k, v) for k, v in dev_dependencies.items()}
-
-    # Write back to pyproject.toml
-    with open('pyproject.toml', 'w') as f:
-        toml.dump(pyproject_data, f)
-
-    print("pyproject.toml updated successfully.")
+def test_python():
+    """Test the python package."""
+    logger.info("Running deployment test.")
+    # Add this directory to the PYTHONPATH for the call to pytest
+    # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    # sys.path.append(os.path.dirname(os.path.abspath(__file__)) + f"/{APP_NAME}")
+    rc = pytest.main(["-x", "tests"])
+    if rc != 0:
+        logger.critical("Error: tests failed.")
+        raise SystemExit("Tests failed.")
+    return rc
 
 
 def get_central_infra_outputs() -> dict:
-    cloudformation = boto3.client("cloudformation")
+    """Get the outputs from the central infrastructure stack."""
+    cloudformation = BOTO_SESSION.client("cloudformation")
     response = cloudformation.describe_stacks(StackName=CENTRAL_INFRA_STACK_NAME)
     values = {item["OutputKey"]: item["OutputValue"] for item in response["Stacks"][0]["Outputs"]}
     # sort dictionary by key
@@ -153,64 +142,98 @@ def get_central_infra_outputs() -> dict:
     return values
 
 
-def attach_common_policy(role_name: str, _policy_arn: str, dry_run: bool = True) -> None:
-    """Attach the common access policy to the role based on the stage. The role is created by chalice, the specific stage's policy ARN is retrieved from the central infrastructure stack."""
-    if dry_run:
-        logger.warning("Dry run enabled. Not attaching policy.")
-        return
-    logger.info(f"Attaching {_policy_arn} to {role_name}")
-    iam = boto3.client("iam")
-    iam.attach_role_policy(RoleName=role_name, PolicyArn=_policy_arn)
+# def attach_common_policy(role_name: str, _policy_arn: str, dry_run: bool = True) -> None:
+#     """Attach the common access policy to the role based on the stage. The role is created by chalice, the specific stage's policy ARN is retrieved from the central infrastructure stack."""
+#     if dry_run:
+#         logger.warning("Dry run enabled. Not attaching policy.")
+#         return
+#     logger.info(f"Attaching {_policy_arn} to {role_name}")
+#     iam = boto3.client("iam")
+#     iam.attach_role_policy(RoleName=role_name, PolicyArn=_policy_arn)
 
 
 if __name__ == "__main__":
+    stages = ['dev', 'prod']
     parser = argparse.ArgumentParser(
         description="Utility script for deployment of python packages and supporting infrastructure.")
-    parser.add_argument("--dry-run", "-d", action="store_true")
-    parser.add_argument("--verbose", "-v", action="store_true")
-    parser.add_argument("--print-central-infra-outputs", '-p', action="store_true")
+    # Tuning
+    parser.add_argument("--dry-run", "-d", action="store_true", help="Prevents any changes from being made.")
+    parser.add_argument("--verbose", "-v", action="store_true", help="Logs debug messages.")
+    parser.add_argument("--print-central-infra-outputs", '-p', action="store_true", help="Prints the central infrastructure outputs as they currently exist.")
+    parser.add_argument("--stage", "-s", default="dev", choices=stages, help=f"If 'dev', publish to pypi-test, if 'prod', publish to pypi.")
 
-    parser.add_argument("--test-python", "-T", action="store_true")
-    parser.add_argument("--update-python-reqs", "-R", action="store_true")
-    parser.add_argument("--package-python", "-P", action="store_true")
-    parser.add_argument("--distribute-python", "-D", action="store_true")
+    # Harmless
+    parser.add_argument("--test-python", "-t", action="store_true", help="Run pytest tests")
+    parser.add_argument("--skip-tests", action="store_true", help="Skip pytest tests")
+    parser.add_argument("--skip-build", action="store_true", help="Skip poetry build commands")
 
-    parser.add_argument("--deploy-infra", "-I", action="store_true")
+    # Dangerous
+    parser.add_argument("--build-python", "-B", action="store_true", help="Run poetry build commands in an idempotent manner.")
+    parser.add_argument("--publish-python", "-P", action="store_true", help="Publish python using poetry")
+    parser.add_argument("--deploy-infra", "-I", action="store_true", help="Deploy the central infrastructure stack ")
 
-    # SETUP
+    # Setup type hints and auto-complete
     args = parser.parse_args()
-    if args.distribute_python:
-        args.package_python = True
-    if args.package_python:
-        args.test_python = True
-    if args.test_python:
-        args.update_python_reqs = True
+    args_skip_tests: bool = args.skip_tests
+    arg_dry_run: bool = args.dry_run
+    arg_verbose: bool = args.verbose
+    args_stage: str = args.stage
+    arg_print_central_infra_outputs: bool = args.print_central_infra_outputs
+    arg_test_python: bool = args.test_python
+    arg_build_python: bool = args.build_python
+    arg_publish_python: bool = args.publish_python
+    arg_deploy_infra: bool = args.deploy_infra
 
-    if args.verbose:
+    # Setup logging
+    if arg_verbose:
         logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.INFO)
 
+    if arg_publish_python:
+        arg_build_python = True
+    if arg_build_python:
+        arg_test_python = True
+
+    log_message = []
+    log_message.append("Taking the following actions:")
+    if arg_print_central_infra_outputs:
+        log_message.append("  - Print central infrastructure outputs")
+    if arg_test_python:
+        log_message.append("  - Test python")
+    if arg_build_python:
+        log_message.append("  - Build python")
+    if arg_publish_python:
+        log_message.append("  - Publish python")
+    if arg_deploy_infra:
+        log_message.append("  - Deploy infrastructure")
+
+    if args.skip_tests:
+        arg_test_python = False
+    if args.skip_build:
+        arg_build_python = False
+    if arg_verbose:
+        print('\n'.join(log_message))
+
     central_infra_outputs = get_central_infra_outputs()
-    if args.print_central_infra_outputs:
+    if arg_print_central_infra_outputs:
         pprint.pprint(central_infra_outputs, indent=4)
 
     # Business logic starts here
 
     # INFRA
-    if args.deploy_infra:
-        deploy_infra()
-        exit(0)
+    if arg_deploy_infra:
+        deploy_infra(dry_run=arg_dry_run)
 
     # PYTHON
-    if args.update_python_reqs:
-        update_pyproject_from_pipfile()
+
+    if arg_test_python:
+        test_python()
+
+    if arg_build_python:
+        build_python()
         exit(0)
 
-    if args.package_python:
-        package_python()
-        exit(0)
-
-    if args.distribute_python:
-        distribute_python()
+    if arg_publish_python:
+        publish_python(stage=args_stage)
         exit(0)
