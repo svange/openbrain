290ce13f176e6a188745b7bacfe736278766b78f
Samuel Vange <7166607+svange@users.noreply.github.com>
2023-09-20 23:35:00 -0700

    Version bump to fix failed deploy

--- openbrain/__init__.py
+++ openbrain/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "0.1.1" # DO NOT EDIT THIS LINE MANUALLY. LET poetry-bumpversion UTILITY DO IT
+__version__ = "0.1.2" # DO NOT EDIT THIS LINE MANUALLY. LET poetry-bumpversion UTILITY DO IT
