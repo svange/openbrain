3ad3042a2350a187162085e44ec8534198868a9e
Samuel Vange <7166607+svange@users.noreply.github.com>
2023-10-16 15:24:04 -0700

    fix: fixed __init__.py environment auto-setup for ci/cd

--- openbrain/__init__.py
+++ openbrain/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.18.1"
 
 import os
 
-if not os.path.exists(".env"):
+if not os.path.exists(".env") and os.environ.get("MODE") is None:
     os.environ["MODE"] = "LOCAL"
 
     print(f"Did not find .env file or env vars, setting MODE=LOCAL in env var.")
