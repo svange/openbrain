6881e3d49e6b37d65a80c9d4d87c1352613d07cc
Samuel Vange <7166607+svange@users.noreply.github.com>
2024-07-25 00:10:11 -0700

    feat: adding automatic documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,25 +2,32 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import openbrain
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../openbrain'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'OpenBra.in'
 copyright = '2024, Samuel Vange'
 author = 'Samuel Vange'
-version = openbrain.__version__
+# version = openbrain.__version__
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx_click",
     "sphinx.ext.autodoc",
-]
+    "sphinx.ext.autosummary",  # Add this line
 
+]
+autosummary_generate = True
 templates_path = ['_templates']
 exclude_patterns = []
 
+
+# Add the path to the openbrain module
 
 
 # -- Options for HTML output -------------------------------------------------
